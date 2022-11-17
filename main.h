8eea476b09c90ab90fcdb9949e8583ebc559016d
Fikri <fikreynursyabani@gmail.com>
2022-11-17 11:31:26 +0700

    add cover

---
+++ main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void cetakMenu(){
+    printf("+----------------------------------+\n");
+    printf("|                                  |\n");
+    printf("|               THE                |\n");
+    printf("|              DINGDUT             |\n");
+    printf("|                                  |\n");
+    printf("+----------------------------------+\n");
+    printf("\nPress any key to continue..");
+    getchar();
+}

ad11b5537dde1cade2944cc8ffe5b23227c8d0d3
Fikri <fikreynursyabani@gmail.com>
2022-11-19 19:46:00 +0700

    update

--- main.c
+++ main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <conio.h>
 #include "main.h"
 
-int main(){
+int main()
+{
+    int opsi, opsiDimensi;
+    char pemain1[50], pemain2[50];
+
+    cetakCover();
     cetakMenu();
-    
+    printf("Pilih Opsi Menu \t: ");
+    scanf("%d", &opsi);
+    switch (opsi)
+    {
+    case 1:
+        printf("Input Nama Pemain 1 \t: ");
+        scanf("%s", &pemain1);
+        printf("Input Nama Pemain 2 \t: ");
+        scanf("%s", &pemain2);
+        break;
+    case 2:
+        cetakMenu2();
+        printf("Pilih Opsi Dimensi \t: ");
+        scanf("%d", &opsiDimensi);
+    case 99:
+        system("cls");
+        printf("+----------------------------------+\n");
+        printf("|                                  |\n");
+        printf("|               TERIMA             |\n");
+        printf("|               KASIH              |\n");
+        printf("|                                  |\n");
+        printf("+----------------------------------+\n");
+        break;
+    default:
+        system("cls");
+        printf("\nInvalid Input");
+        getchar();
+        break;
+    }
 }
--- main.h
+++ main.h
@@ -1,12 +1,32 @@
-#include <stdio.h>
-
-void cetakMenu(){
+void cetakCover(){
     printf("+----------------------------------+\n");
     printf("|                                  |\n");
     printf("|               THE                |\n");
     printf("|              DINGDUT             |\n");
     printf("|                                  |\n");
     printf("+----------------------------------+\n");
-    printf("\nPress any key to continue..");
+    printf("\nPress 'Enter' to continue..");
     getchar();
+}
+
+void cetakMenu(){
+    system("cls");
+    printf("+----------------------------------+\n");
+    printf("|Menu :                            |\n");
+    printf("|1. Ganti Nama Pemain              |\n");
+    printf("|2. Mulai Main                     |\n");
+    printf("|3. Developers                     |\n");
+    printf("|99. Keluar Program                |\n");
+    printf("+----------------------------------+\n");
+}
+
+void cetakMenu2(){
+    system("cls");
+    printf("+----------------------------------+\n");
+    printf("|Menu :                            |\n");
+    printf("|1. 3 x 3                          |\n");
+    printf("|2. 5 x 5                          |\n");
+    printf("|3. 7 x 7                          |\n");
+    printf("|99. Keluar Program                |\n");
+    printf("+----------------------------------+\n");
 }
